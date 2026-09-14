f6217544862e816161aab588b51844fc391e512b
Harley D. Eades III <harley.eades@pm.me>
2026-09-14 15:49:20 -0400

    Adding a new Text spec for unit tests.

---
+++ test/Data/HoleyExp/TextSpec.hs
@@ -0,0 +1,37 @@
+{-|
+Module      : HExpInternalSpec
+Description : Testing spec for the holey expressions API
+Copyright   : (c) Harley Eades, 2026
+              (c) W⋊B, 2026
+Maintainer  : harley.eades@gmail.com
+
+Various properties of the holey-expressions API.
+-}
+module  Data.HoleyExp.TextSpec (spec) where
+
+import Data.HoleyExp.HExpInternal
+import Data.HoleyExp.Text
+import Test.QuickCheck.HExp                ()
+
+import Test.Hspec            
+import Test.Helpers                        (parseTest)
+import Test.QuickCheck                     (Property
+                                           ,Testable (property))
+import Test.Hspec.QuickCheck               (prop)
+import Test.Helpers                        (UnitTest(..)
+                                           ,test_case)
+
+spec :: Spec 
+spec = do
+    describe "Unit Tests:" $ do
+        test_case "example test case" test_example
+
+testParseHExp :: Parser (HExp Text Text)
+testParseHExp = hExpParser
+
+test_example :: UnitTest (Maybe (HExp Text Text))
+test_example = UnitTest {
+         test_result=parseTest testParseHExp "foo$1(aabar"
+        ,test_output=Nothing
+    }
+
